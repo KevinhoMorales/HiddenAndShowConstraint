8ea54fad36f5b060fbfd0374475ce7999a32e532
Kevin Morales <kfmorales94@gmail.com>
2020-06-20 18:37:25 -0500

    First Upload

--- Hidden/ViewController.swift
+++ Hidden/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var first: UILabel!
+    @IBOutlet weak var second: UILabel!
+    @IBOutlet weak var third: UILabel!
+    @IBOutlet weak var heightConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func show(_ sender: UIButton) {
+        second.isHidden = false
+        heightConstraint.constant = 110
+        print(heightConstraint.constant)
+    }
+    
+    @IBAction func hidden(_ sender: UIButton) {
+        second.isHidden = true
+        heightConstraint.constant = 70
+        print(heightConstraint.constant)
+    }
+    
+    
 }
 
